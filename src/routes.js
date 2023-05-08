fef856e48aa83ed4bd3815127981499e70660e8a
Dwi Aji Cahyono <dwiaji724@gmail.com>
2023-05-08 20:15:56 +0700

    backend final with validation reviewer

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,32 @@
+const {
+  addBooksHandler, getAllBooks, getDetailBooks, getEditBooks, getDeleteBook,
+} = require('./handler');
+
 const routes = [
   {
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return('hello world');
-    },
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler,
   },
   {
     method: 'GET',
-    path: '/about',
-    handler: (request, h) => {
-      return('hello world');
-    },
+    path: '/books',
+    handler: getAllBooks,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getDetailBooks,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: getEditBooks,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: getDeleteBook,
   },
 ];
 
